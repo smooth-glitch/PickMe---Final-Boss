067f47dd231be35538c181132febfdce46206197
Arjun <arjunsridhar445@gmail.com>
2026-01-09 15:38:29 +0530

    fixed firebase config

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@
 
 window.APP_CONFIG = {
   firebaseConfig: {
-    apiKey: "1a6f9527a68c7052a00ef73d587eff62",
+    apiKey: "AIzaSyB8fW-bBx26cMPUDjZH6Xnuqrsw5Nkizu4",
     authDomain: "movienight-picker.firebaseapp.com",
     projectId: "movienight-picker",
     storageBucket: "movienight-picker.firebasestorage.app",
     messagingSenderId: "169239854984",
     appId: "1:169239854984:web:a25b26739ed82a37f4c43d",
-    measurementId: "G-3P11RRJQ4K",
+    measurementId: "G-3P11RRJQ4K"
   },
 };
 
