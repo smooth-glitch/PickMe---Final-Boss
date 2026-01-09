938b0efa94fba7518347da9e8963b1a54e0d51bd
Arjun <arjunsridhar445@gmail.com>
2026-01-09 15:34:27 +0530

    added firebase config

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// TMDB + Firebase config
+
+window.APP_CONFIG = {
+  firebaseConfig: {
+    apiKey: "1a6f9527a68c7052a00ef73d587eff62",
+    authDomain: "movienight-picker.firebaseapp.com",
+    projectId: "movienight-picker",
+    storageBucket: "movienight-picker.firebasestorage.app",
+    messagingSenderId: "169239854984",
+    appId: "1:169239854984:web:a25b26739ed82a37f4c43d",
+    measurementId: "G-3P11RRJQ4K",
+  },
+};
+
+window.APPCONFIG = {
+  firebaseConfig: window.APP_CONFIG.firebaseConfig,
+};
